72b37bd7a84911763b65487f8d72e8787d2e4c29
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-03-23 17:17:32 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+
+'use strict'
+/**
+* This function calculates area of a square
+*/
+function calculate () {
+ // input
+ const sidea = parseInt(document.getElementById('side-a').value)
+ const sideb = parseInt(document.getElementById('side-b').value)
+
+
+ // process
+ const area = sidea * sideb
+
+
+ // output
+ document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
+
+
